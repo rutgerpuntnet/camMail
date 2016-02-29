e45f36ee37c8ea636f820ca42906f1ec747f1269
rutger <github@rutger.net>
2016-02-29 23:32:45 +0100

    added ffmpeg call

--- src/main/java/net/rutger/HomePage.java
+++ src/main/java/net/rutger/HomePage.java
@@ -4,6 +4,9 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
+import java.io.IOException;
+import java.util.Date;
+
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
@@ -14,5 +17,18 @@ public class HomePage extends WebPage {
 
 		// TODO Add your page's components here
 
-    }
+		try {
+			long startmillis = System.currentTimeMillis();
+			Process process = Runtime.getRuntime().exec("/Users/rutger/Downloads/FFMpeg/ffmpeg -y -i rtsp://192.168.1.10:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream -ss 00:00:01.500 -f image2 -vframes 1 /tmp/thumb.jpg");
+			int exitValue = process.waitFor();
+			long totalmillis = System.currentTimeMillis() - startmillis;
+			System.out.println("ffmpeg call in ms:" + totalmillis + " with exit value " + exitValue);
+
+		} catch (IOException e) {
+			System.out.println("IOException on ffmpeg call");
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException on ffmpeg call");
+		}
+
+	}
 }

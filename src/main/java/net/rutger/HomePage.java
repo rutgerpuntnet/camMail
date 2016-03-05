a4fcb64fe3ce26a656c5771791d2a77108a72306
rutger <github@rutger.net>
2016-03-05 18:32:20 +0100

    layout and others

--- src/main/java/net/rutger/HomePage.java
+++ src/main/java/net/rutger/HomePage.java
@@ -1,10 +1,18 @@
 package net.rutger;
 
+import org.apache.log4j.Logger;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.DynamicImageResource;
+import org.apache.wicket.request.resource.IResource;
+
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -12,50 +20,57 @@ import java.io.IOException;
 import java.util.Date;
 
 public class HomePage extends WebPage {
+	private static final String THUMBNAIL_LOCATION = "/tmp/imageFile.jpg";
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(HomePage.class);
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
 		try {
 			long startmillis = System.currentTimeMillis();
-			Process process = Runtime.getRuntime().exec("/Users/rutger/Downloads/FFMpeg/ffmpeg -y -i rtsp://192.168.1.10:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream -ss 00:00:01.500 -f image2 -vframes 1 /tmp/thumb.jpg");
 
-			// TODO check/wait for new date/time in file
-//			File tempFile=new File("/tmp/thumb.jpg");
-//			File renamedFile=new File("/tmp/thumbLatest.jpg");
-//			while(!tempFile.renameTo(renamedFile)); //try re-naming the file which is being encoded by ffmpeg
-//			Date date = new Date(file.lastModified());
-
-			long lastmodified = System.currentTimeMillis();
-			try {
-				Thread.sleep(100);
-			} catch(InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
-			while(lastmodified <= System.currentTimeMillis()) {
-				File file = new File("/tmp/thumb.jpg");
-				lastmodified = file.lastModified();
-			}
+			ProcessBuilder pb = new ProcessBuilder("frontdoorimage.sh", THUMBNAIL_LOCATION, "1");
+			logger.debug("Run command");
+			Process process = pb.start();
+			int errCode = process.waitFor();
 
 
-			System.out.println("Encoding done");
+			logger.debug("Encoding done:" + errCode);
 
 			long totalmillis = System.currentTimeMillis() - startmillis;
-			System.out.println("ffmpeg call in ms:" + totalmillis);
+			logger.debug("ffmpeg call in ms:" + totalmillis);
 
+			final byte[] thumbnail = getImage(THUMBNAIL_LOCATION);
+			logger.debug("Thumbnail byte length = " + thumbnail.length);
+
+			IResource imageResource = new DynamicImageResource() {
+				@Override
+				protected byte[] getImageData(IResource.Attributes attributes) {
+					return thumbnail;
+				}
+			};
+			Image image = new Image("thumbnail", imageResource);
+
+			Link reloadLink = new Link("reload") {
+				@Override
+				public void onClick() {
+					setResponsePage(new HomePage(new PageParameters()));
+				}
+			};
+			reloadLink.add(image);
+			this.add(reloadLink);
 		} catch (IOException e) {
-			System.out.println("IOException on ffmpeg call");
+			logger.error("IOException on ffmpeg call", e);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 	}
 
 
-	private byte[] getImage(File imageFile) {
+	private byte[] getImage(String fileNamePath) {
+		File imageFile = new File(fileNamePath);
 		BufferedImage img = null;
 		byte[] bytes;
 		try {
@@ -64,9 +79,27 @@ public class HomePage extends WebPage {
 			ImageIO.write(img, "jpg", baos);
 			bytes = baos.toByteArray();
 		} catch (IOException e) {
-			System.out.println("failed to read image");
-			return null;
+			logger.error("failed to read image");
+			return new byte[0];
 		}
 		return bytes;
+	}
+
+	public static ServletContext getContext(){
+
+		WebApplication webApplication = WebApplication.get();
+		if(webApplication!=null){
+			ServletContext servletContext = webApplication.getServletContext();
+			if(servletContext!=null){
+				return servletContext;
+			}else{
+				//do nothing
+			}
+		}else{
+			//do nothing
+		}
+
+		return null;
+
 	}
 }
